a463c37d4c9eb65d37825f6ec3a31b9b2e4d814e
giogalbuchi <ggalbuchi@gmail.com>
2020-11-16 17:08:33 -0300

    Worked on login page and styles

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Router, Switch, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import "./style/main.scss";
+
+import history from './history';
+
+import SignIn from './components/auth/signin';
+import SignUp from './components/auth/signup';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Layout>
-          <p>hey this is a child component</p>
-          <h2>hey there</h2>
+          <Switch>
+            <Route path='/' exact component={SignIn}/>
+            <Route path='/signin' exact component={SignIn}/>
+            <Route path='/signup' exact component={SignUp}/>
+          </Switch>
         </Layout> 
-      </BrowserRouter>
+      </Router>
     </Provider>,
     document.querySelector(".app-wrapper")
   );
